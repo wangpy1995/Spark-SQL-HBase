86176b4bcc20fb5e864874ea84f840c21d188f17
pw <592114387@qq.com>
2022-10-31 18:07:51 +0800

    [MOD]优化HBase建表与插入数据代码结构并测试

--- src/test/scala/org/apache/spark/sql/hbase/TConstants.scala
+++ src/test/scala/org/apache/spark/sql/hbase/TConstants.scala
@@ -7,26 +7,42 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 /**
-  * Created by wpy on 17-5-13.
-  */
+ * Created by wpy on 17-5-13.
+ */
 private[hbase] object TConstants {
   private[hbase] lazy val conf = new Configuration()
   private[hbase] lazy val hConf = HBaseConfiguration.create(conf)
   private[hbase] lazy val conn = ConnectionFactory.createConnection(hConf)
   private[hbase] lazy val admin = conn.getAdmin
 
-  private[hbase] lazy val sparkConf = new SparkConf().setMaster("local[*]").setAppName("test").set("spark.hadoopRDD.ignoreEmptySplits","false").set("spark.driver.port","4042")
-  private[hbase] lazy val ss = SparkSession.builder().config(sparkConf).getOrCreate()
+  private[hbase] lazy val sparkConf = new SparkConf()
+    .setMaster("local[*]")
+    .setAppName("test")
+    .set("spark.hadoopRDD.ignoreEmptySplits", "false")
+    .set("spark.driver.port", "4042")
+
+  private[hbase] lazy val ss = SparkSession.builder()
+    .config(sparkConf)
+    .getOrCreate()
+
   private[hbase] lazy val sc = ss.sparkContext
   private[hbase] lazy val extraConfig = Map(
-    "schema.file.url" -> "/home/wpy/IdeaProjects/Spark-SQL-HBase/src/main/resources/test.yml",
+    "schema.file.url" -> "/home/pw/IdeaProjects/Spark-SQL-HBase/src/main/resources/test.yml",
     "spark.hbase.client.impl" -> "org.apache.spark.sql.hbase.client.HBaseClientImpl")
   private[hbase] lazy val hs = new HBaseSession(
     TConstants.sc,
     conf,
     extraConfig)
 
+  //namespace and table's name
+  private[hbase] lazy val TEST_NAMESPACE = "pw"
   private[hbase] lazy val TEST_TABLE_NAME = "test"
-  private[hbase] lazy val TEST_NAMESPACE = "wpy1"
+  //column family
+  private[hbase] lazy val TEST_COL_A = "A"
+  private[hbase] lazy val TEST_COL_B = "B"
+  //max count of rows
+  private[hbase] lazy val MAX_ROW_CNT = 1000
+  //max count of qualifiers
+  private[hbase] lazy val MAX_QUALIFIER_CNT = 10
 
 }
--- src/test/scala/org/apache/spark/sql/hbase/client/TestHBase.scala
+++ src/test/scala/org/apache/spark/sql/hbase/client/TestHBase.scala
@@ -1,84 +1,140 @@
 package org.apache.spark.sql.hbase.client
 
-import org.apache.hadoop.hbase.{NamespaceDescriptor, TableName}
-import org.apache.hadoop.hbase.client.{ColumnFamilyDescriptorBuilder, Put, Scan, TableDescriptorBuilder}
+import org.apache.hadoop.hbase.client.{Admin, ColumnFamilyDescriptorBuilder, Connection, Put, Scan, TableDescriptorBuilder}
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{NamespaceDescriptor, TableName}
+import org.apache.spark.internal.Logging
 import org.apache.spark.sql.hbase.TConstants
 import org.scalatest.funsuite.AnyFunSuite
 
 /**
  * Created by wpy on 17-5-18.
  */
-class TestHBase extends AnyFunSuite {
-  private val admin = TConstants.admin
-  private val conn = TConstants.conn
+class TestHBase extends AnyFunSuite with Logging {
 
-  def dbExists(dbName: String): Boolean = {
+  private lazy val table = TConstants.TEST_NAMESPACE + ":" + TConstants.TEST_TABLE_NAME
+
+  def dbExists(admin: Admin, dbName: String): Boolean = {
     admin.listNamespaceDescriptors.exists(_.getName == dbName)
   }
 
-  def tableExists(table: String): Boolean = {
+  def tableExists(admin: Admin, table: String): Boolean = {
     admin.tableExists(TableName.valueOf(table))
   }
 
-  def createUserNamespaceAndTable(): Unit = {
-    val namespaceName = "wpy1"
-    val tableName = "wpy1:test"
+  def createUserNamespaceAndTable(admin: Admin, namespaceName: String,
+                                  table: String, cols: String*): Unit = {
     if (!admin.listNamespaces().contains(namespaceName)) {
       val namespace = NamespaceDescriptor.create(namespaceName).build()
       admin.createNamespace(namespace)
     }
-    if (!admin.tableExists(TableName.valueOf(tableName))) {
-      val tableDesc = TableDescriptorBuilder.newBuilder(TableName.valueOf("wpy1:test"))
-      val cf1 = ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes("cf1")).build()
-      val cf2 = ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes("cf2")).build()
-      tableDesc.setColumnFamily(cf1)
-      tableDesc.setColumnFamily(cf2)
+    if (!admin.tableExists(TableName.valueOf(table))) {
+      val tableDesc = TableDescriptorBuilder.newBuilder(TableName.valueOf(table))
+      cols.foreach { colName =>
+        val col = ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(colName)).build()
+        tableDesc.setColumnFamily(col)
+      }
       admin.createTable(tableDesc.build())
-      conn.close()
+      admin.close()
+      logInfo(s"Succeed in creating table $table")
     }
   }
 
-  def insertData(): Unit = {
-    for (i <- 0 until 1000) {
-      val rowKey = i.formatted("%04d").toString
+  def insertData(conn: Connection, tableName: String,
+                 maxRowCnt: Int, maxQualifierCnt: Int,
+                 splitChar: String, columnNames: String*): Unit = {
+    val bytesColumnNames = columnNames.map(Bytes.toBytes)
+
+    //前面凑0对齐
+    val rowPattern = s"%0${math.log10(maxRowCnt).toInt + 1}d"
+    logInfo(s"Format Row with: $rowPattern")
+    val qualifierPattern = s"%0${math.log10(maxQualifierCnt).toInt + 1}d"
+    logInfo(s"Format qualifier with: $qualifierPattern")
+    val splitterBytes = Bytes.toBytes(splitChar)
+    val tableConn = conn.getTable(TableName.valueOf(tableName))
+
+    def insertData(put: Put, bytesRowKey: Array[Byte], bytesColumnName: Array[Byte],
+                   formattedQualifier: String, splitterBytes: Array[Byte]) = {
+      //pattern: col+${splitter}+qualifier, A_01
+      val qualifierBytes = Bytes.add(bytesColumnName, splitterBytes, Bytes.toBytes(formattedQualifier))
+      //pattern: col+${splitter}+qualifier+${splitter}+rowKey, A_01_0001
+      val value = Bytes.add(qualifierBytes, splitterBytes, bytesRowKey)
+      put.addColumn(bytesColumnName, qualifierBytes, value)
+      put
+    }
+
+    for (i <- 0 until TConstants.MAX_ROW_CNT) {
+      val rowKey = i.toString.format(rowPattern)
+      val bytesRowKey = Bytes.toBytes(rowKey)
       val put = new Put(Bytes.toBytes(rowKey))
-      for (j <- 0 until 10) {
 
-        put.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes(s"cf1_$j"),
-          Bytes.toBytes(i + "column_family1_" + "_" + j + "_" + rowKey))
-
-        put.addColumn(Bytes.toBytes("cf2"), Bytes.toBytes(s"cf2_$j"),
-          Bytes.toBytes(i + "column_family2_" + "_" + j + "_" + rowKey))
-
-        conn.getTable(TableName.valueOf("wpy1:test")).put(put)
+      for (j <- 0 until TConstants.MAX_QUALIFIER_CNT) {
+        val formattedQualifier = j.toString.format(qualifierPattern)
+        //col value pattern: qualifier_rowKey
+        bytesColumnNames.foreach { bytesColumnName =>
+          insertData(put, bytesRowKey, bytesColumnName, formattedQualifier, splitterBytes)
+        }
+        tableConn.put(put)
       }
     }
+    logInfo(s"Succeed in inserting ${TConstants.MAX_ROW_CNT} rows")
+    tableConn.close()
     conn.close()
   }
 
-  def scan(): Unit = {
+  def scan(conn: Connection, tableName: String, cols: String*): Unit = {
+
     import scala.collection.JavaConverters._
+
     val scan = new Scan()
     //    scan.withStartRow(Bytes.toBytes("")).withStopRow(Bytes.toBytes(""))
-    scan.addFamily(Bytes.toBytes("cf1"))
-    scan.addFamily(Bytes.toBytes("cf2"))
-    val cnt = conn.getTable(TableName.valueOf("wpy1:test")).getScanner(scan).iterator().asScala.count(_ => true)
-    println(cnt)
+    scan.addFamily(Bytes.toBytes(TConstants.TEST_COL_A))
+    scan.addFamily(Bytes.toBytes(TConstants.TEST_COL_B))
+    val cnt = conn.getTable(TableName.valueOf(tableName)).getScanner(scan).iterator().asScala.count(_ => true)
+    logInfo(s"max row numbers: $cnt")
     conn.close()
+  }
+
+  def dropTable(admin: Admin, table: String): Unit = {
+    val tableName = TableName.valueOf(table)
+    while (admin.tableExists(tableName)) {
+      while (!admin.isTableDisabled(tableName)) {
+        admin.disableTable(tableName)
+      }
+      admin.deleteTable(tableName)
+    }
+    logInfo(s"Succeed in dropping table: $table")
+    admin.close()
   }
 
 
   test("create namespace and table") {
-    createUserNamespaceAndTable()
+    createUserNamespaceAndTable(
+      TConstants.admin,
+      TConstants.TEST_NAMESPACE,
+      table,
+      TConstants.TEST_COL_A,
+      TConstants.TEST_COL_B)
   }
 
   test("insert some data") {
-    insertData()
+    insertData(TConstants.conn, table,
+      1000,
+      10,
+      "_",
+      TConstants.TEST_COL_A,
+      TConstants.TEST_COL_B)
   }
 
   test("scan") {
-    scan()
+    scan(TConstants.conn,
+      table,
+      TConstants.TEST_COL_A,
+      TConstants.TEST_COL_B)
+  }
+
+  test("delete") {
+    dropTable(TConstants.admin, table)
   }
 
 
@@ -88,8 +144,11 @@ object TestHBase {
   val testHBase = new TestHBase()
 
   def main(args: Array[String]): Unit = {
-    testHBase.createUserNamespaceAndTable()
-    testHBase.insertData()
-    testHBase.scan()
+    testHBase.createUserNamespaceAndTable(TConstants.admin, TConstants.TEST_NAMESPACE, testHBase.table)
+    testHBase.insertData(TConstants.conn, testHBase.table,
+      1000, 10, "_",
+      TConstants.TEST_COL_A,
+      TConstants.TEST_COL_B)
+    testHBase.scan(TConstants.conn, testHBase.table, TConstants.TEST_COL_A, TConstants.TEST_COL_B)
   }
 }
